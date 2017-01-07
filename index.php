5cc2255c81d99548db100ec82c2c550d58e830c7
Earl-Grey8 <mike@lostfables.com>
2017-01-07 13:58:06 +0000

    General Update

--- index.php
+++ index.php
@@ -9,6 +9,50 @@
 	<title>Lost Fables</title>
 </head>
 <body>
+
+			<div class="banner"></div>
+<div class="custom-profilecontainer">
+			<div class="row">
+				<div class="custom-profilehead"></div>
+				<br />
+				<h1>Lost Fables</h1>
+				<div class="container">
+					<div class="col-sm-4">
+						<div class="custom-contenttable">
+						<i class="fa fa-envelope-o fa-5x" aria-hidden="true"></i>
+						<h2>Get Updates</h2>
+						<p>Want to find out when we're launching? You can sign up to our Newsletter</p>
+						<a href=""><p>Click Here</p></a>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="custom-contenttable">
+						<i class="fa fa-puzzle-piece fa-5x" aria-hidden="true"></i>
+						<h2>Contribute</h2>
+						<p>Can you offer your help? Coding, Artwork? Get in touch, we're happy to hear what you have to offer!</p>
+						<p>Email us: mail@lostfables.com</p>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="custom-contenttable">
+						<i class="fa fa-shield fa-5x" aria-hidden="true"></i><br />
+						<h2>Early Adopter</h2>
+						<p>Working on a project? Want to be one of the first to get involved?</p>
+						<a href="welcome.php"><b>Click Here</b></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- <div class="row">
+				<br />
+				<div class="col-sm-1">
+				</div>
+				<div class="col-sm-10">
+				<h2 style="text-align: center;">Recent Uploads</h2>
+				</div>
+				<div class="col-sm-1">
+				</div>
+			</div> -->
 	
 	<center>
 	<img src="images/comingsoon/lostfables.png" style="width: 700px;"> <br />
@@ -16,10 +60,10 @@
 	<div class="col-sm-2">
 		</div>
 		<div class="col-sm-4">
-		<i class="fa fa-envelope-o fa-2x" aria-hidden="true"> <b>Email</b>: mail@lostfables.com</i>
+		<i class="fa fa-envelope-o fa-2x" aria-hidden="true"> mail@lostfables.com</i>
 		</div>
 		<div class="col-sm-4">
-		<i class="fa fa-twitter-square fa-2x" aria-hidden="true"> @LostFables</i></p>
+		<i class="fa fa-twitter-square fa-2x" aria-hidden="true"> @LostFables</i>
 		</div>
 		<div class="col-sm-2">
 		</div>
--- welcome.php
+++ welcome.php
@@ -23,15 +23,12 @@
 				<p style="text-align: center;">A message from The Founder</p>
 				</div>
 				<div class="col-sm-10">
-				<p class="letter" style="text-align: center; font-size: 40px;">Lost Fables cordially invites you to the 'Book of Tales' update;</p>
+				<p class="letter" style="text-align: center; font-size: 40px;">Lost Fables cordially invites you to the 'Indie Collab' update;</p>
 				<h2>Dear Reader,</h2>
-					<p>"Since I began Lost Fables in 2015, a lot has evolved with it. As with any new project there are many initial plans which didn't make the cut and new ideas took their place. As the project has evolved, so have my own skills in web development meaning this project did exactly what I set out for it to do. Help me to improve myself with something fun and interactive. It is now that I have come this far that I wish to redevelop exactly what Lost Fables is.</p>
-					<p>The first version of Lost Fables started out as a place for people to share publications with one another, while the intention was sound, the execution was somewhat lacking. The site lay dorment for some time until the point my other project 'Novel Tea Games' was ready to release it's Visual Novel 'Runeous' in which Lost Fables became the 'Publishing Entity.' I soon moved on to help others in their Visual Novel publishing and evolved the site around that.</p>
-					<p>Publishing was simply something to give purpose to the site during it's 'down-time' for development and now it's time to renew the original idea and turn Lost Fables into the site it was meant to be but with a little more 'flare'.</p>
-					<p>I am officially welcoming you to help with the testing phase by becoming a Beta-Tester to the site. As a personal 'thank-you' those who stick around and help throughout the entire testing process will be added to a 'premium' list and will benefit from any 'Premium Membership' perks which may be introduced in the future.</p>
-					<p>It's easy to help out since all you will need to do is upload your content, test features and comment on what you think works well, could be improved upon or should be implemented. You will be able to shape the way the site looks and functions, becoming a key part of this community!"</p>
+					<p>"Lost Fables is working on setting up a website for indie developers to share content, connect and collaborate. For those not developing, we are incorporating a 'follow' system so that members can keep up to date with the developers that interest them.</p>
+					<p>While we have most of the groundwork in place, there are still several features we're working on. You can follow our roadmap <a href="https://trello.com/b/ZyEGUH6z/lost-fables-roadmap">here</a>. We are looking for early adopters to help us test our features and begin submitting content ready for the 'grand opening' if you are interested then please contact mail@lostfables.com and let us know you're interested! Be sure to tell us a bit about yourself and your project(s).</p>
+					<p>Finally I personally want to thank you for your interest in becoming a part of our new community!</p>
 					<p class="letter" style="font-size: 25px; text-align: right;">~ Earl Grey</p>
-					<p><a href="test.php">Enter Website</p>
 				</div>
 			</div>
 			<div class="custom-footer">
@@ -39,6 +36,5 @@
 			</div>
 		</div>
 	</div>
-			<?php include("template/main/footer.php"); ?>
 </body>
 </html>
